f1b3fd85bcabf2c7898ee713633742779c1b87f5
Igor Santos <igor.ox13@gmail.com>
2022-08-31 23:11:38 -0300

    logica basica C#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int i;
+// int i2 = i = 10;
+// //se o valor de "i2" for o mesmo que 42 é true(saida 10) se for false saida 20.
+// i = i2 == 42 ? 10 : 20;
+// Console.WriteLine(i2);
 
-int i;
-int i2 = i = 10;
-
-//se o valor de "i2" for o mesmo que 42 é true(saida 10) se for false saida 20.
-i = i2 == 42 ? 10 : 20;
-
-Console.WriteLine(i2);
+Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Ola {name}!");
+Console.WriteLine("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($"Você tem {age} anos.");
