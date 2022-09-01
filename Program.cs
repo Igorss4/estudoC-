433fcaeb626cfbbe523e7008aa082232aae93302
Igor Santos <igor.ox13@gmail.com>
2022-08-31 23:40:53 -0300

    array com for

--- Program.cs
+++ Program.cs
@@ -3,25 +3,35 @@
 // //se o valor de "i2" for o mesmo que 42 é true(saida 10) se for false saida 20.
 // i = i2 == 42 ? 10 : 20;
 // Console.WriteLine(i2);
+// Console.WriteLine("Digite seu nome: ");
+// string name = Console.ReadLine();
+// Console.WriteLine($"Ola {name}!");
+// Console.WriteLine("Digite o ano do seu nascimento: ");
+// int year = int.Parse(Console.ReadLine());
+// int age = 2022 - year;
+// Console.WriteLine($"Você tem {age} anos.");
 
-Console.WriteLine("Digite seu nome: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Ola {name}!");
-Console.WriteLine("Digite o ano do seu nascimento: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2022 - year;
-Console.WriteLine($"Você tem {age} anos.");
+// if (age >= 17)
+// {
+//     Console.WriteLine("Você é maior de idade!");
+// }
+// else if (age == 12)
+// {
+//     Console.WriteLine("Você tem 12 anos!");
+// }
+// else
+// {
+//     Console.WriteLine("Você é menor de idade!");
+// }
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Fred", "Maria", "Igor" };
 
-if (age >= 17)
-{
-    Console.WriteLine("Você é maior de idade!");
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+    }
 }
-else if (age == 12)
-{
-    Console.WriteLine("Você tem 12 anos!");
-}
-else
-{
-    Console.WriteLine("Você é menor de idade!");
-}
-
