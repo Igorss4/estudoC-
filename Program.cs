f381a495cd8581dfc1827607f9aa16141f92e756
Igor Santos <igor.ox13@gmail.com>
2022-08-31 23:19:26 -0300

    if else

--- Program.cs
+++ Program.cs
@@ -11,3 +11,17 @@ Console.WriteLine("Digite o ano do seu nascimento: ");
 int year = int.Parse(Console.ReadLine());
 int age = 2022 - year;
 Console.WriteLine($"Você tem {age} anos.");
+
+if (age >= 17)
+{
+    Console.WriteLine("Você é maior de idade!");
+}
+else if (age == 12)
+{
+    Console.WriteLine("Você tem 12 anos!");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade!");
+}
+
